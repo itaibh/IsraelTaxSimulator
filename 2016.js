11461e3d9a3f3390d1345b3526334c0ed269e7d3
Itai Bar-Haim <itaibh@gmail.com>
2016-03-30 20:22:13 +0300

    Implemented basic calculation, added 2016 data.

---
+++ 2016.js
@@ -0,0 +1,14 @@
+var tax_details = tax_details || {};
+
+tax_details[2016] = {
+    tax_levels: [
+        {min: 0, tax: 0.10},
+        {min: 62641, tax: 0.14},
+        {min: 107041, tax: 0.21},
+        {min: 166321, tax: 0.31},
+        {min: 237601, tax: 0.34},
+        {min: 496921, tax: 0.48},
+        {min: 803521, tax: 0.50},
+    ],
+    bonus_point_value:216,
+}
