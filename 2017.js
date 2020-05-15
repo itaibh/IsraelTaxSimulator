df8bb52f4ad7dabc554bebe22f62381c6dba6b74
Itai Bar-Haim <itaibh@gmail.com>
2020-05-15 09:22:05 +0300

    Fixed 2016 and added 2017-2019

---
+++ 2017.js
@@ -0,0 +1,17 @@
+var tax_details = tax_details || {};
+
+tax_details[2017] = {
+    tax_levels: [
+        {min: 0, tax: 0.10, additive: 0},
+        {min: 74641, tax: 0.14, additive: 7464},
+        {min: 107041, tax: 0.20, additive: 12000},
+        {min: 171841, tax: 0.31, additive: 24960},
+        {min: 238801, tax: 0.35, additive: 45718},
+        {min: 496921, tax: 0.47, additive: 136060},
+        {min: 639997, tax: 0.50, additive: 203305},
+    ],
+    bonus_point_value:2580,
+    max_pension:7308,
+    pension_discount: 0.35,
+    donation_discout: 0.35,
+}
